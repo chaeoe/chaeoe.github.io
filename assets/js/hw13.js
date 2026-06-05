d6371c621d48a488d7dffaf17a317452bf615d2a
chaeoe <chaeyoung@snu.ac.kr>
2026-06-05 03:58:22 +0000

    과제13 마무리 (2)

--- assets/js/hw13.js
+++ assets/js/hw13.js
@@ -19,7 +19,7 @@ function getWords(text) {
     return text
         .toLowerCase()
         .replace(/[.,!?;:'"‘’“”()\[\]_*-]/g, " ")
-        // 상위 30개 어휘에 --가 등장해서, 이를 방지하기 위해 기존 [...]에 -를 추가함.
+        // Dracula의 상위 30개 어휘 중 --가 등장해서, 이를 방지하기 위해 기존 [...]에 -를 추가함.
         .split(/\s+/)
         .filter(w => w.length > 0);
 }
