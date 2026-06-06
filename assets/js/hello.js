661adb2d0ddc9f745d717b866d45764c83b4a5bc
chaeoe <chaeyoung@snu.ac.kr>
2026-06-06 21:07:23 +0000

    hello.js 내용 보완

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -3,7 +3,5 @@ const name = "김채영";
 const major = "아동가족학";
 const year = 2026;
 
-const message = `안녕하세요! ${name}(${major} 전공)의 사이트입니다.
-이 사이트는 ${year}년에 개설되었습니다.`;
-
-console.log(message);
+console.log(`안녕하세요! ${name}(${major} 전공)의 사이트입니다.`);
+console.log(`이 사이트는 ${year}년에 개설되었습니다.`);
